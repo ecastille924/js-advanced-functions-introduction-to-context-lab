17b6d2206d052e16e678ebe02ebb9d539ffa6039
Erick Castille <ecastille924@gmail.com>
2021-07-12 18:45:04 -0400

    commit tree 1a66ec73ec896893beac942b57d46e1259de5c42

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function createEmployeeRecord(array){
+    let record 
+    return record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+
+    }
+}
+
+function createEmployeeRecords(arrays){
+    return arrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(employeeRecord, date){
+    const newTimeInEvent = {
+        type: "TimeIn",
+        date: parseInt(date.slice(-4)),
+        hour: date.slice(0, 10)
+    }
+    employeeRecord.timeInEvents.push(newTimeInEvent)
+    return employeeRecord
+}
